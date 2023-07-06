0eea802c1ab34d66060582da3b4e20b1bc907b49
kanggeon123 <kanggeon216@gmail.com>
2023-07-05 21:57:49 -0700

    created prompt questions for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,59 @@ var useDigit = true;
 var useSpecial = true;
 var length = 0;
 
+function promptCriteria() {
+  while(criteria != 1 && criteria != 2 && criteria != 3) {
+    criteria = prompt("Which criteria should be included in the password? 1: Length of Password, 2: Character Types, 3: Both", length);
+  }
+  if(criteria == 1)
+    lengthCriteria = true;
+  else if (criteria == 2) {
+    length = Math.floor(Math.random() * 120) + 8;
+    characterTypesCriteria = true;
+  }
+  else {
+    lengthCriteria = true;
+    characterTypesCriteria = true;
+  } 
+  if(lengthCriteria) {
+    while (length < 8 || length > 128 || isNaN(length)){
+      length = parseInt(prompt("What is the length of the password","Between 8 and 128"))
+    }
+  }
+  if(characterTypesCriteria){
+    var ans = "";
+    while(ans != "yes" && ans != "no"){
+      ans = prompt("Should the password include lowercase?","yes or no");
+    }
+    if(ans == "no") {
+      useLower = false;
+    }
+    ans = "";
+    while(ans != "yes" && ans != "no"){
+      ans = prompt("Should the password include uppercase?","yes or no");
+    }
+    if(ans == "no") {
+      useUpper = false;
+    }
+    ans = "";
+    while(ans != "yes" && ans != "no"){
+      ans = prompt("Should the password include digits?","yes or no");
+    }
+    if(ans == "no") {
+      useDigit = false;
+    }
+    ans = "";
+    while(ans != "yes" && ans != "no"){
+      ans = prompt("Should the password include Special Characters?","yes or no");
+    }
+    if(ans == "no") {
+      useSpecial = false;
+    }
+    ans = "";
+    console.log(categories);
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
